20022de09ba54046a5096b6cfb65fb34189c3459
guilhermeSENAI <guilherme.b.nery6@aluno.senai.br>
2025-11-12 10:16:12 -0300

    feature 1

--- script.js
+++ script.js
@@ -10,4 +10,41 @@ window.document.addEventListener("DOMContentLoaded", () => {
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `RESULTADO: ${input}`;
   });
-});
+
+//   document.getElementById("btnPrimeiraMaiuscula").addEventListener("click", () => {
+//     let input = document.getElementById("input-text").value.toLowerCase();
+//     let resultado = document.getElementById("resultado");
+//     input = input.charAt(0).toUpperCase() + input.slice(1);
+//     for(i=0; i<=input.length;i++){
+//       if (input.charAt(i)==="." && input.charAt(i+1)===" "){
+//         input[i+2] = input.charAt(i+2).toUpperCase()
+//       }
+//     }
+//     resultado.innerHTML = `RESULTADO: ${input[i+2]}`;
+
+//   })
+
+// });
+document.getElementById('btnPrimeiraMaiuscula').addEventListener('click', () => {
+    let input = document.getElementById('input-text').value.toLowerCase();
+    let resultado = document.getElementById('resultado');
+
+    
+    input = input.charAt(0).toUpperCase() + input.slice(1);
+
+    
+    let newText = '';
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) === '.' && input.charAt(i + 1) === ' ' && i + 2 < input.length) {
+            newText += input.charAt(i); 
+            newText += input.charAt(i + 1); 
+            newText += input.charAt(i + 2).toUpperCase(); 
+            i += 2; 
+        } else {
+            newText += input.charAt(i); 
+        }
+    }
+    input = newText; 
+
+    resultado.innerHTML = `RESULTADO: ${input}`;
+})});
